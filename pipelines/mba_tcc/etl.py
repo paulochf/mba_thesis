44b2569bf40c25e65eb800667a55361089f6aedb
Paulo Haddad <paulochf@gmail.com>
2022-08-01 03:21:45 -0300

    feat: add task for creating dataset file index + tests

--- pipelines/mba_tcc/etl.py
+++ pipelines/mba_tcc/etl.py
@@ -6,13 +6,45 @@ from prefect_dask import DaskTaskRunner
 from utils.config import get_env_var_as_path
 
 
-@task(retries=3)
-def prepare_file(path):
-    pass
+@task(
+    description="Create file index, extracting the parts from the file name.",
+    tags=["index", "final"],
+    version="1",
+)
+def make_files_index(data_paths: List[Path], output_dir: Path) -> bool:
+    import pandas as pd
+
+    paths_as_strs = map(str, data_paths)
+    files_df = pd.DataFrame(paths_as_strs, columns=["path"])
+    files_df["filename"] = files_df.path.str.split("/").str[-1]
+
+    name_parts_columns = [
+        "file_number",
+        "mnemonic",
+        "training_end",
+        "anomaly_start",
+        "anomaly_end",
+    ]
+    files_df[name_parts_columns] = files_df.filename.str.extract(
+        "([0-9]+)"
+        "_UCR_Anomaly_"
+        "([A-Za-z0-9]+)"
+        "_([0-9]+)"
+        "_([0-9]+)"
+        "_([0-9]+)"
+        ".txt"
+    )
+
+    files_df = files_df.set_index("file_number").sort_index()
+    files_df.to_parquet(output_dir / "files_index.parquet", index=False)
+
+    return True
 
 
 @flow(task_runner=DaskTaskRunner())
-def prepare_files(paths: List[Path]):
-    # for path in paths:
-    #     prepare_file.submit(path)
-    pass
+def prepare_files(data_paths: List[Path]):
+    final_path = get_env_var_as_path("PATH_DATA_FINAL")
+
+    make_files_index(data_paths, final_path)
+    # for file_path in data_paths:
+    #     make_files_index.submit(file_path)
---
+++ pipelines/tests/test_etl.py
@@ -0,0 +1,26 @@
+import logging
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pandas as pd
+from mba_tcc.etl import make_files_index
+from utils.config import get_env_var_as_path
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test_make_files_index():
+    dataset_path = get_env_var_as_path("PATH_DATA_RAW_UCR")
+    dataset_files = dataset_path.glob("./*.txt")
+    final_path = get_env_var_as_path("PATH_DATA_FINAL")
+
+    with TemporaryDirectory(dir=final_path, suffix="_test") as tmp_dir:
+        path_tmp_dir = Path(tmp_dir)
+        make_files_index.fn(dataset_files, path_tmp_dir)
+
+        index_file_path = path_tmp_dir / "files_index.parquet"
+        assert index_file_path.exists()
+        assert index_file_path.is_file()
+
+        df = pd.read_parquet(index_file_path)
+        assert len(df) == 250
