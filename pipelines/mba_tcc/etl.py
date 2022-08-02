088e38d660a034a41f7d61b8c742c6c37ac33d22
Paulo Haddad <paulochf@gmail.com>
2022-08-02 02:53:40 -0300

    feat(etl): add file fixing step

--- pipelines/mba_tcc/etl.py
+++ pipelines/mba_tcc/etl.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from pathlib import Path
 from typing import List
 
@@ -15,36 +16,76 @@ def make_files_index(data_paths: List[Path], output_dir: Path) -> bool:
     import pandas as pd
 
     paths_as_strs = map(str, data_paths)
-    files_df = pd.DataFrame(paths_as_strs, columns=["path"])
-    files_df["filename"] = files_df.path.str.split("/").str[-1]
+    files_df = pd.DataFrame(paths_as_strs, columns=["file_path"])
 
-    name_parts_columns = [
-        "file_number",
-        "mnemonic",
-        "training_end",
-        "anomaly_start",
-        "anomaly_end",
-    ]
-    files_df[name_parts_columns] = files_df.filename.str.extract(
-        "([0-9]+)"
-        "_UCR_Anomaly_"
-        "([A-Za-z0-9]+)"
-        "_([0-9]+)"
-        "_([0-9]+)"
-        "_([0-9]+)"
-        ".txt"
+    # Get the string after the last '/'.
+    files_df["file_name"] = files_df.file_path.str.split("/").str[-1]
+
+    # File name example: 012_UCR_Anomaly_tiltAPB1_100000_114283_114350.txt
+    # Parts:
+    #   - 012          : dataset number
+    #   - _UCR_Anomaly_: fixed string
+    #   - tiltAPB1     : mnemonic name
+    #   - 100000       : training data index range between [1, X]
+    #   - 114283_114350: anomaly data index range between [114283; 114350]
+    name_parts_columns = ["file_number", "mnemonic", "training_index_end", "anomaly_index_start", "anomaly_index_end"]
+    numeric_columns = [x for x in name_parts_columns if x != "mnemonic"]
+
+    # Extract each regex group into a column.
+    files_df[name_parts_columns] = files_df.file_name.str.extract(
+        r"([0-9]+)"  # dataset number
+        r"_UCR_Anomaly_"  # fixed string
+        r"([A-Za-z0-9]+)"  # mnemonic name
+        r"_([0-9]+)"  # training data end index
+        r"_([0-9]+)"  # anomaly data start index
+        r"_([0-9]+)"  # anomaly data end index
+        r"\.txt"  # file extension
     )
 
-    files_df = files_df.set_index("file_number").sort_index()
-    files_df.to_parquet(output_dir / "files_index.parquet", index=False)
+    files_df = files_df.astype({c: "int64" for c in numeric_columns})  # Convert the numeric columns from to numeric.
+    files_df = files_df.set_index("file_number").sort_index()  # File number column as dataframe sorted index.
+    files_df.to_parquet(output_dir / "files_index.parquet")  # Save dataframe as parquet in the final data folder.
 
     return True
 
 
+@task()
+def fix_file(file_number: int, file_path: Path, file_name: str, output_path: Path, *args, **kwargs):
+    import pandas as pd
+
+    files_fix = {204, 205, 206, 207, 208, 225, 226, 242, 243}
+    file_to_fix = file_number in files_fix
+
+    df = pd.read_csv(file_path, header=None, delim_whitespace=file_to_fix)
+
+    if file_to_fix:
+        df = df.T
+
+    df = df.rename(columns={0: "vals"})
+    df.to_parquet((output_path / file_name).with_suffix(".parquet"))
+
+
 @flow(task_runner=DaskTaskRunner())
+def fix_files():
+    import pandas as pd
+
+    interim_path = get_env_var_as_path("PATH_DATA_INTERIM")
+    final_path = get_env_var_as_path("PATH_DATA_FINAL")
+
+    index_path = final_path / "files_index.parquet"
+    index_df = pd.read_parquet(index_path)
+
+    output_path = interim_path / "0__raw_to_parquet"
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    row: namedtuple
+    for row in index_df.reset_index().itertuples():
+        fix_file(**row._asdict(), output_path=output_path)
+
+
+@flow()
 def prepare_files(data_paths: List[Path]):
     final_path = get_env_var_as_path("PATH_DATA_FINAL")
 
     make_files_index(data_paths, final_path)
-    # for file_path in data_paths:
-    #     make_files_index.submit(file_path)
+    fix_files()
